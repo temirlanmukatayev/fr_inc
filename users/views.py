ed5b2f21a5b4a09c2ccdd0c07bf98c307922d863
Temirlan <temirlanmukatayev@gmail.com>
2024-04-20 16:48:42 +0500

    signup ready

--- users/views.py
+++ users/views.py
@@ -1,5 +1,19 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+
+from .forms import CustomUserCreationForm
+
+
+User = get_user_model()
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class SignUpView(CreateView):
+    template_name = 'registration/signup.html'
+    model = User
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
